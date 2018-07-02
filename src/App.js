f52400b47dccf7d8e8a341983c5a02a45fa591b6
mohammed <mohammedalshahri9@gmail.com>
2018-07-02 17:59:20 +0300

    V2 of this app

--- src/App.js
+++ src/App.js
@@ -1,88 +1,84 @@
 import React, { Component } from 'react';
 import './App.css';
+
 import Slidemenu from './slidemenu'
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
-
-
 
 class App extends Component {
   state={
     myMarker:[
       {
       id : "4bb324ee42959c74f790212c",
-      name:' Assaraya Turkish Restaurant',
-      contact:'044994',
-       lat: 24.692822,
-       lng: 46.695275
+      name:' ',
+      contact:'',
+      position:{ lat: 24.692822, lng: 46.695275}
      },
      {
       id : "4bb7ae81cf2fc9b6e99e9e02",
-       name:'king fahad medical city',
-       contact:'949494',
-        lat: 24.688967,
-        lng: 46.703240
+       name:'',
+       contact:'',
+       position:{ lat: 24.688967, lng: 46.703240}
+
       },
       {
-      id : "4db599fc81543d71da5b8e83",
-        name:'Centria Mall',
-        contact:'04949494',
-         lat: 24.697504,
-         lng: 46.683967
+        id : "4db599fc81543d71da5b8e83",
+        name:'',
+        contact:'',
+        position:{  lat: 24.697504,lng: 46.683967}
+
 
        },
       {
-      id : "4bb3aa0c14cfd13a455f16ab",
-         name:'Fairouz Garden Restaurant',
-         contact:'9499494',
-          lat: 24.707841,
-          lng: 46.675313
+        id : "4bb3aa0c14cfd13a455f16ab",
+        name:'',
+        contact:'',
+        position:{   lat: 24.707841,lng: 46.675313}
+
         },
         {
         id : "4bb60acd46d4a5932198c5c0",
-          name:'Panorama Mall',
-          contact:'9494833',
-           lat: 24.692855,
-           lng: 46.669626
+        name:'',
+        contact:'',
+        position:{lat: 24.692855,lng: 46.669626}
+
          }
       ],
       showingInfoWindow: false,
       activeMarker: {},
       selectedPlace: {},
-       place:[]
-  }
 
+  }
+//fetch the foursquare API to bring data
   componentDidMount() {
     const temp = this.state.myMarker
         temp.map((ress)=>{
 return(
-      fetch('https://api.foursquare.com/v2/venues/'+ress.id+"?&client_secret=SPWJKXYP5XDFFSSBCTZ3F0NQQZXMAZB1L0MRXNWAN3WODKR0&client_id=3BJNU223FDTYXD4W4HZBWCDXHEIL3LXIYOD3U230BZQEHBDS&v=20180629")
+      fetch('https://api.foursquare.com/v2/venues/'+ress.id+"?&client_secret=N2KRN3O5KSZYGNYCQOVPYLL5OCYIBOARJEEI21DA242S4BDD&client_id=OTTPGOIVHLELWJ5IKGI2M0I2THW3ABXFP3RN24E2J30A5KGN&v=20180629")
       .then(res => res.json())
       .then((result) => {
+        if(result && result.response){
         ress.name =result.response.venue.name
         ress.contact = result.response.venue.contact.formattedPhone
-
-       })
-
+      }}).catch(re=>{
+        console.log("There are error in lodeing data please try agane");
+      })
      )
-
     })
        this.setState({myMarker:temp})
-
-
   }
 
-
+  //when marker clicked this open InfoWindow
   onMarkerClick = (location) =>{
     console.log(location);
   this.setState({
-    position: {lat:location.position.lat, lng:location.position.lng },
+    position: {lat:location.position.lat , lng:location.position.lng },
     showingInfoWindow: true,
-    name: location.title,
-    contact: location.cont
+    name: location.name,
+    contact: location.contact
   });
 }
 
-
+//clic anywhere in map to close InfoWindow
      onMapClicked = (props) => {
    if (this.state.showingInfoWindow) {
      this.setState({
@@ -91,9 +87,10 @@ return(
      })
    }
  };
+
+
+
   render() {
-  console.log(this.state.myMarker);
-console.log(this.state.selectedPlace);
     return (
         <div className="App">
         <Map google={this.props.google}
@@ -105,8 +102,9 @@ console.log(this.state.selectedPlace);
         }}
         zoom={15} >
         {this.state.myMarker.map((res)=>{
+
           return(
-        <Marker key={res.id} title={res.name} position={{lat:res.lat ,lng:res.lng}} onClick={this.onMarkerClick} cont={res.contact}/>
+        <Marker key={res.id} name={res.name} position={res.position} onClick={this.onMarkerClick} contact={res.contact}/>
          )
        })}
 
@@ -114,25 +112,17 @@ console.log(this.state.selectedPlace);
                position={this.state.position}
                visible={this.state.showingInfoWindow}>
                   <div>
-               <h7>Name: {this.state.name}</h7>
-               <h7>Name: {this.state.contact}</h7>
+               <h3 className={"heading"} >Name: {this.state.name}</h3>
+               <h3 className={"heading"}>contact Number: {this.state.contact}</h3>
                </div>
              </InfoWindow>
 
         </Map>
-        <Slidemenu loc ={this.state.myMarker}/>
+        <Slidemenu onMarkerClick={this.onMarkerClick} loc={this.state.myMarker}/>
         </div>
     );
   }
-
-
-
-
-
-
-
 }
-
 export default GoogleApiWrapper({
   apiKey: ('AIzaSyCS7x4GKH0ecNQKouvoF0kBkF11cmWMez8')
 })(App)
--- src/slidemenu.js
+++ src/slidemenu.js
@@ -1,34 +1,43 @@
 import React, { Component } from 'react';
 import { slide as Menu } from 'react-burger-menu'
  import './App.css';
+ import escapeRegExp from 'escape-string-regexp'
 export default class slidemenu extends Component {
 
-  // state = {
-  //           query: this.props.loc,
-  //           locatioNnedd:{}
-  //       }
+        state ={
+            query:''
+        }
 
-   //
-   // findPlace=(e)=>{
-   //   if(!e || e ===''){
-   //          this.setState({query: []});
-   //          return;
-   //  }
-   //      const p = this.state.query.filter(loca => e !== loca.name)
-   //      this.setState({locatioNnedd:p})
-   // }
+ findloc= (query)=>{
+   this.setState({query: query.trim()})
 
-
-
-
+ }
   render () {
+        let location
+
+        if(this.state.query){
+        const match = new RegExp(escapeRegExp(this.state.query) , 'i')
+        location =  this.props.loc.filter((cont)=> match.test(cont.name) )
+        }else{
+        location =  this.props.loc;
+        }
+
+   console.log(this.state.query);
     return (
       <Menu >
-       <h1 className="menu-title">Select one of the list to see it!</h1>
+          <input
+          aria-required="true"
+          className="Search-bar"
+          type="text"
+          value={this.state.query}
+          onChange={(e)=>this.findloc(e.target.value)}
+          placeholder="Search about place"/>
         <ol className="list-menu">
-        {this.props.loc.map((res)=>{
+
+
+        {location.map((res)=>{
           return(
-          <li className="list" key={res.id}><a className="link" href="">{res.name}</a></li>
+         <button key={res.id} onClick={()=>this.props.onMarkerClick(res ,res.name)}    className="list">{res.name}</button>
          )
        })}
         </ol>
